672058bdf84f7bfaced7dde53c3a0546ccce8869
taegangyun <taegangyun@gmail.com>
2024-06-28 17:23:43 +0900

    Initializing code in SPawn.h, cpp

--- Source/StudyProject/Private/Character/SPawn.cpp
+++ Source/StudyProject/Private/Character/SPawn.cpp
@@ -2,12 +2,38 @@
 
 
 #include "Character/SPawn.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
 
 // Sets default values
 ASPawn::ASPawn()
 {
 
 	PrimaryActorTick.bCanEverTick = false;
+
+	float CharacterHalfHeight = 90.f;
+	float CharacterRadius = 40.f;
+
+#pragma region InitializeCapsuleComponent
+
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+	SetRootComponent(CapsuleComponent);
+	CapsuleComponent->SetCapsuleHalfHeight(CharacterHalfHeight);
+	CapsuleComponent->SetCapsuleRadius(CharacterRadius);
+
+#pragma endregion
+
+#pragma region InitializeSkeletalMeshComponent
+
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
+	SkeletalMeshComponent->SetupAttachment(RootComponent);
+
+
+#pragma endregion
+
+
 
 }
 
--- Source/StudyProject/Public/Character/SPawn.h
+++ Source/StudyProject/Public/Character/SPawn.h
@@ -6,6 +6,14 @@
 #include "GameFramework/Pawn.h"
 #include "SPawn.generated.h"
 
+
+class UCapsuleComponent;
+class USkeletalMeshComponent;
+class UFloatingPawnMovement;
+class USpringArmComponent;
+class UCameraComponent;
+
+
 UCLASS()
 class STUDYPROJECT_API ASPawn : public APawn
 {
@@ -15,5 +23,20 @@ public:
 	
 	ASPawn();
 
+protected:
 
+	UPROPERTY(EditAnywhere, Category = "SPawn", Meta = (AllowPrivateAccess))
+	TObjectPtr<UCapsuleComponent> CapsuleComponent;
+
+	UPROPERTY(EditAnywhere, Category = "SPawn", Meta = (AllowPrivateAccess))
+	TObjectPtr<USkeletalMeshComponent> SkeletalMeshComponent;
+
+	UPROPERTY(EditAnywhere, Category = "SPawn", Meta = (AllowPrivateAccess))
+	TObjectPtr<UFloatingPawnMovement> FloatingPawnMovement;
+
+	UPROPERTY(EditAnywhere, Category = "SPawn", Meta = (AllowPrivateAccess))
+	TObjectPtr<USpringArmComponent> SpringArmComponent;
+
+	UPROPERTY(EditAnywhere, Category = "SPawn", Meta = (AllowPrivateAccess))
+	TObjectPtr<UCameraComponent> CameraComponent;
 };
