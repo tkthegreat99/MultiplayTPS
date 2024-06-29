cbdfe33f6ab7354c22abd6d26f6e0cef06c39930
taegangyun <taegangyun@gmail.com>
2024-06-29 16:21:37 +0900

    Spawn Initiize

--- Source/StudyProject/Private/Character/SPawn.cpp
+++ Source/StudyProject/Private/Character/SPawn.cpp
@@ -7,7 +7,7 @@
 #include "Camera/CameraComponent.h"
 #include "GameFramework/FloatingPawnMovement.h"
 
-// Sets default values
+
 ASPawn::ASPawn()
 {
 
@@ -29,11 +29,25 @@ ASPawn::ASPawn()
 
 	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
 	SkeletalMeshComponent->SetupAttachment(RootComponent);
-
+	FVector PivotPosition(0.f, 0.f, -CharacterHalfHeight);
+	FRotator PivotRotation(0.f, -90.f, 0.f);
+	SkeletalMeshComponent->SetRelativeLocationAndRotation(PivotPosition, PivotRotation);
 
 #pragma endregion
 
+#pragma region InitializeCamera
 
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	SpringArmComponent->TargetArmLength = 400.f;
+	SpringArmComponent->SetRelativeRotation(FRotator( - 15.f, 0.f, 0.f));
+
+	CameraComponent->CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+
+#pragma endregion
+
+	FloatingPawnMovement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("FloatingPawnMovement"));
 
 }
 
